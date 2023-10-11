e59959849d553a3bf15675c7e7df6b0b9bb0dd79
Ernie <139922039+ballinjedi@users.noreply.github.com>
2023-10-11 15:18:49 -0400

    added html css and some javascript

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+var mainNav = document.getElementById("main-nav")
+document.getElementById("open-button").addEventListener("click",function (){
+    mainNav.style.display = "block"
+})
+
+document.getElementById("close-button").addEventListener("click",function (){
+   mainNav.style.display = "none"
+})
