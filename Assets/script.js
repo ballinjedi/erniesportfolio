f4753ad9bf96e2d1ab7ce1d5ce6c4b39aa2548f8
Ernie <139922039+ballinjedi@users.noreply.github.com>
2023-10-11 21:32:13 -0400

    added more code and style changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,18 @@
 var mainNav = document.getElementById("main-nav")
+var closeButton = document.getElementById("close-button")
+var SHOW = "show"
 document.getElementById("open-button").addEventListener("click",function (){
-    mainNav.style.display = "block"
+    // mainNav.style.display = "block"
+    mainNav.classList.add(SHOW)
 })
 
-document.getElementById("close-button").addEventListener("click",function (){
-   mainNav.style.display = "none"
+closeButton.addEventListener("click",function (){
+//    mainNav.style.display = "none"
+   mainNav.classList.remove(SHOW)
+})
+
+Array.from(mainNav.querySelectorAll("a")).forEach(a => {
+    a.addEventListener("click",()=>{
+        closeButton.click()
+    })
 })
