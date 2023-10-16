379413102f7f623064ef59f97682353a90f44393
Ernie <139922039+ballinjedi@users.noreply.github.com>
2023-10-16 17:46:38 -0400

    changes to css and js

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,30 @@
-var mainNav = document.getElementById("main-nav")
-var closeButton = document.getElementById("close-button")
-var SHOW = "show"
-document.getElementById("open-button").addEventListener("click",function (){
+const mainNav = document.getElementById("main-nav");
+const closeButton = document.getElementById("close-button");
+const SHOW = "show";
+const navAnchors = mainNav.querySelectorAll("a");
+
+document.getElementById("open-button").addEventListener("click", () => {
     // mainNav.style.display = "block"
     mainNav.classList.add(SHOW)
 })
 
-closeButton.addEventListener("click",function (){
-//    mainNav.style.display = "none"
-   mainNav.classList.remove(SHOW)
+closeButton.addEventListener("click", () => {
+    //    mainNav.style.display = "none"
+    mainNav.classList.remove(SHOW)
 })
 
-Array.from(mainNav.querySelectorAll("a")).forEach(a => {
-    a.addEventListener("click",()=>{
+Array.from(navAnchors).forEach(a => {
+    a.addEventListener("click", event => {
+        event.preventDefault()
         closeButton.click()
+        const href = a.getAttribute("href");
+        if (href.startsWith("#")) {
+            const element = document.querySelector(href);
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
+            }
+        }
     })
+
 })
+
